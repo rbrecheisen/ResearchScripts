49867f06f2d70542f0414d80ac6ef84a1f4621a2
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2024-07-16 13:35:26 +0200

    Saving work

---
+++ src/ReorganizeWeasisOutput.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+import pydicom
+import pydicom.errors
+
+ROOTDIR = 'C:\\Users\\r.brecheisen\\Downloads\\weasis\\lung3'
+OUTPUTDIR = 'C:\\Users\\r.brecheisen\\Downloads\\weasis\\lung3reorg'
+
+
+def is_dicom_file(dicom_file_path):
+    try:
+        if os.path.isfile(dicom_file_path):
+            if os.path.split(dicom_file_path)[1] != 'DICOMDIR':
+                pydicom.dcmread(dicom_file_path, stop_before_pixels=True)
+                return True
+        return False
+    except pydicom.errors.InvalidDicomError:
+        return False
+
+
+def main():
+
+    os.makedirs(OUTPUTDIR, exist_ok=False)
+    
+    for subject_dir_name in os.listdir(ROOTDIR):
+        subject_dir_path = os.path.join(ROOTDIR, subject_dir_name)
+        for root, dirs, files in os.walk(subject_dir_path):
+            for dicom_file_name in files:
+                dicom_file_path = os.path.join(root, dicom_file_name)
+                if is_dicom_file(dicom_file_path):
+                    output_dicom_file_path = os.path.join(OUTPUTDIR, subject_dir_name + '.dcm')
+                    shutil.copyfile(dicom_file_path, output_dicom_file_path)
+                    print(subject_dir_name)
+
+
+if __name__ == '__main__':
+    main()
